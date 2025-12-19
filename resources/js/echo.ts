186eb74149148c0eaa6bb0f321e812ab0d7ea7e9
KristopherZlo <kostos2001@gmail.com>
2025-12-19 19:40:32 +0200

    refactor(echo): migrate module to TS

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -3,12 +3,15 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
+const wsPort = Number(import.meta.env.VITE_REVERB_PORT ?? 80);
+const wssPort = Number(import.meta.env.VITE_REVERB_PORT ?? 443);
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    wsPort,
+    wssPort,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
