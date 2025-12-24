b3d5d5124b3cd5ca60b8b914c417a102521eb7f6
KristopherZlo <kostos2001@gmail.com>
2025-12-24 02:31:10 +0200

    echo bootstrap tidy, minor cleanup

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -9,7 +9,7 @@ Alpine.start();
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
- * allow your team to quickly build robust real-time web applications.
+ * allow teams to quickly build robust real-time web applications.
  */
 
 // ОТКЛЮЧАЕМ стандартный echo.js, чтобы не было второй конфигурации
