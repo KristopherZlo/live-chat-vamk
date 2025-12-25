544e5c6e4f2d96265b0084f38de33ad408da55f7
KristopherZlo <kostos2001@gmail.com>
2025-12-25 10:19:46 +0200

    Use Alpine CSP build

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import Alpine from 'alpinejs';
+import Alpine from 'alpinejs/dist/csp';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
